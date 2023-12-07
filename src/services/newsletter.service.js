09081f50ebcc13aa1c5400e3a341cc70854ee596
orhanince <ince.orhan@yandex.com>
2023-12-07 17:45:17 +0300

    Add missing newsletter controller and service methods

--- src/services/newsletter.service.js
+++ src/services/newsletter.service.js
@@ -1,45 +1,177 @@
-const _ = require('lodash');
 const { Newsletter } = require('../models');
 const GenericError = require('../utils/generic-error');
 const { v4: uuidv4 } = require('uuid');
 const moment = require('moment-timezone');
-const cryptoService = require('./crypto.service');
-const jwt = require('../utils/jwt');
 const paginationOptionGenerator = require('../utils/pagination-option-generator');
-const sequelizeClient = require('./../utils/sequelize-client');
-const { default: slugify } = require('slugify');
+
 /**
- * Get all users
+ * Get all newsletters
  * @param pagination
- * @param AUTH
  * @returns {Promise<{data: Promise<Model[]> | Promise<any[]>, count: *, status: boolean}>}
  */
-async function getAll() {
-  const newsletterLists = await sequelizeClient.query('SELECT * FROM newsletter_lists');
-  console.log('newsletter-list', newsletterLists[0]);
+async function getAll({ pagination }) {
+  const options = paginationOptionGenerator({
+    pagination,
+    likeColumns: ['id', 'newsletter_id'],
+    where: {
+      status: true,
+    },
+  });
 
-  for await (const n of newsletterLists[0]) {
-     const now = moment.utc().toISOString();
-     
-     const createNewsletter = await Newsletter.create({
-     newsletter_id: uuidv4(),
-     first_name: n.fname,
-     last_name: n.lname,
-     email: n.email,
-     interests: n.interests,
-     status: 1,
-     created_at: now,
-     updated_at: now,
-     });
-     console.log('createNewsletter', createNewsletter);
-  };
+  const count = await Newsletter.count({
+    where: options.where,
+  });
+
+  const data = await Newsletter.findAll({
+    attributes: ['newsletter_id', 'first_name', 'last_name','email'],
+    options,
+  });
+
   return {
     status: true,
-    data: 1
+    count,
+    data,
   };
+}
 
+/**
+ * Add new newsletter.
+ * @param {string} first_name 
+ * @param {string} last_name 
+ * @param {string} email 
+ * @param {string} interests 
+ * @returns 
+ */
+async function create({ body }) {
+  const { first_name, last_name, email, interests } = body || {};
+  
+  const foundNewsletter = await Newsletter.count({
+    where: {
+      email: email,
+    },
+  });
+
+  if (foundNewsletter) {
+    throw new GenericError(400, '', `Newsletter already exists!`, foundNewsletter);
+  }
+
+  const now = moment.utc().toISOString();
+  const createNewsletter = await Newsletter.create({
+    newsletter_id: uuidv4(),
+    first_name: first_name,
+    last_name: last_name,
+    interests: interests,
+    status: 1,
+    created_at: now,
+    updated_at: now,
+  });
+
+  if (createNewsletter) {
+    return {
+      status: true,
+      data: createNewsletter
+    };
+  }
+}
+
+/**
+ * Get single newsletter by id.
+ * @param req
+ * @returns {Promise<any>}
+ */
+async function getNewsletter({ params }) {
+  const { newsletter_id } = params || {}
+  return await Newsletter.findOne({
+    where: {
+      newsletter_id: newsletter_id
+    }
+  });
+}
+
+/**
+ * Update newsletter by id.
+ * @param req
+ * @returns {Promise<any>}
+ */
+async function update({ params, body }) {
+  const { newsletter_id } = params || {};
+  const { first_name, last_name, email, interests, status } = body || {};
+  
+  const foundNewsletter = await Newsletter.findOne({
+    where: {
+      newsletter_id: newsletter_id
+    }
+  });
+
+  if (!foundNewsletter) {
+    throw new GenericError(400, '', `Newsletter does not exist!`, foundNewsletter);
+  }
+
+  const now = moment.utc().toISOString();
+  const [updateNewsletter] = await Newsletter.update(
+    {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      interests: interests,
+      status: status,
+      updated_at: now,
+    },
+    {
+      where: {
+        newsletter_id: newsletter_id,
+      }
+    }
+  );
+
+  return {
+    status: true,
+    data: updateNewsletter
+  };
+}
+
+/**
+ * Delete newsletter by id.
+ * @param req
+ * @returns {Promise<any>}
+ */
+async function deleteNewsletter({ params }) {
+  const { newsletter_id } = params || {};
+
+  const foundNewsletter = await Newsletter.findOne({
+    where: {
+      newsletter_id: newsletter_id
+    }
+  });
+
+  if (!foundNewsletter) {
+    throw new GenericError(400, '', `Country does not exist!`, foundNewsletter);
+  }
+
+  const now = moment.utc().toISOString();
+  const deleteNewsletter = await Newsletter.update(
+    {
+      status: false,
+      updated_at: now,
+      deleted_at: now,
+    },
+    {
+      where: {
+        newsletter_id: newsletter_id,
+      }
+    }
+  );
+
+  return {
+    status: true,
+    data: deleteNewsletter
+  };
 }
 
 module.exports = {
-  getAll
+  getAll,
+  getNewsletter,
+  create,
+  update,
+  deleteNewsletter
 };
